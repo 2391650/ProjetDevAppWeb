69d9ace285f003abca5b7c91c1e6a5bb1e8f685a
Alexandre Rusenov <2391650@collegemv.qc.ca>
2025-03-05 12:51:10 -0500

    modification de navbar et ajout de footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Navbar from "./Navbar/Navbar";
 import Cards from "./Cards/Cards";
 import "bootstrap/dist/css/bootstrap.css";
+import Footer from "./footer/Footer";
 
 function App() {
   return (
     <>
       <Navbar/>
       <Cards/>
+      <Footer/>
     </>
   );
 }
