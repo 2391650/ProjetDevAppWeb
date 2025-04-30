b88d7852daf903abdd9244bb2d1d8fa036b31142
usernametarek <6256769@collegemv.qc.ca>
2025-04-29 21:33:16 -0400

    Essai de sauvegarde de session : erreur lorsque je crée un groupe je n'arrive pas à lui associer l'id du prof

--- backend/src/main/java/frognest/tech/backend/repositories/ProfRepository.java
+++ backend/src/main/java/frognest/tech/backend/repositories/ProfRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProfRepository extends JpaRepository<Prof, Long> {
     Prof findByFirstname(String firstname);
+    Prof findByIdprof(Long idprof);
 }
